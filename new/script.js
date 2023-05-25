349ae7bded4ad802209820f1a5b2639f212e863b
Rahul Sharma <rockstarrahulsharma213@gmail.com>
2023-05-25 19:00:16 +0530

    new

---
+++ new/script.js
@@ -0,0 +1,17 @@
+let hamburger = document.querySelector( '.hamburger');
+let navLinks = document.getElementById( 'nav-links');
+let links = document.querySelectorAll( '.links');
+
+//Display Links onClick On Hamburger
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('hide');
+    hamburger.classList.toggle('lines-rotate');
+});
+
+// Hide navlink Container onClick any single link
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click',()=>{
+    navLinks.classList.toggle('hide');
+    });
+}
